0e891fef47cfd1e925c9752b6e45b76c0d459581
Nicholas Vilela <nvilela@proforma.com>
2019-08-13 14:42:08 -0400

    Finished the project

--- public/main.js
+++ public/main.js
@@ -1,7 +1,22 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+const API_KEY = 'ab3311611e68c0b21da61c1e8642758f'
+const searchField = document.querySelector('#search-field')
+const searchBtn = document.querySelector('#search-btn')
+const output = document.querySelector('#output')
+
+const getApiUrl = () => {
+  const query = searchField.value
+  const mainPart = 'http://api.openweathermap.org/data/2.5/weather?'
+  const searchQuery = `q=${query}&`
+  const apiKeyPart = `appid=${API_KEY}`
+  const url = `${mainPart}${searchQuery}${apiKeyPart}`  
+  getData(url)
 }
 
-document.addEventListener('DOMContentLoaded', main)
+const getData = (url) => {
+  axios.get(url)
+    .then(res => {
+      const kelvin = res.data.main.temp
+      const fahrenheited = (((kelvin - 273.15) * 9) / 5) + 32
+      output.textContent = `${res.data.name} ${fahrenheited}`
+    })
+}
