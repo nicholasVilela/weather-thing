68dbff7409103804efe05ab85465a50d9214f492
Nicholas Vilela <nvilela@proforma.com>
2019-08-14 13:06:53 -0400

    done

--- public/main.js
+++ public/main.js
@@ -3,20 +3,21 @@ const searchField = document.querySelector('#search-field')
 const searchBtn = document.querySelector('#search-btn')
 const output = document.querySelector('#output')
 
-const getApiUrl = () => {
+const getWeatherApiUrl = () => {
   const query = searchField.value
   const mainPart = 'http://api.openweathermap.org/data/2.5/weather?'
-  const searchQuery = `q=${query}&`
+  const searchQuery = `q=${query}&units=imperial&`
   const apiKeyPart = `appid=${API_KEY}`
-  const url = `${mainPart}${searchQuery}${apiKeyPart}`  
+  const url = `${mainPart}${searchQuery}${apiKeyPart}`
+    
   getData(url)
 }
 
 const getData = (url) => {
   axios.get(url)
     .then(res => {
-      const kelvin = res.data.main.temp
-      const fahrenheited = (((kelvin - 273.15) * 9) / 5) + 32
-      output.textContent = `${res.data.name} ${fahrenheited}`
+      console.log(res)
+      const temp = res.data.main.temp
+      output.textContent = `${res.data.name} ${temp} ${res.data.weather[0].main}`
     })
 }
